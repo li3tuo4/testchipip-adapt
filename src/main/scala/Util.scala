d2faa5d42c6094ce790afe2c77afd96e56ba2340
Tuo Li <sam.of.ee@gmail.com>
2022-09-07 20:40:10 +1000

    Add minimum modifications to be compatible with chisel3 v3.3

--- src/main/scala/Util.scala
+++ src/main/scala/Util.scala
@@ -7,13 +7,15 @@ import freechips.rocketchip.diplomacy.{IdRange, ValName}
 import freechips.rocketchip.util.AsyncResetReg
 import freechips.rocketchip.tilelink._
 
-class ResetSync(c: Clock, lat: Int = 2) extends Module(_clock = c) {
-  val io = IO(new Bundle {
-    val reset = Input(Bool())
-    val reset_sync = Output(Bool())
-  })
-  io.reset_sync := ShiftRegister(io.reset,lat)
-}
+class ResetSync(c: Clock, lat: Int = 2) extends Module {
+   val io = IO(new Bundle {
+     val reset = Input(Bool())
+     val reset_sync = Output(Bool())
+   })
+  withClock(c) {
+   io.reset_sync := ShiftRegister(io.reset,lat)
+  }
+ }
 
 object ResetSync {
   def apply(r: Bool, c: Clock): Bool = {
@@ -58,13 +60,18 @@ case class AsyncWideCounter(width: Int, inc: UInt = 1.U, reset: Boolean = true)
 }
 
 // As WideCounter, but it's a module so it can take arbitrary clocks
-class WideCounterModule(w: Int, inc: UInt = 1.U, reset: Boolean = true, clockSignal: Clock = null, resetSignal: Bool = null)
-    extends Module(Option(clockSignal), Option(resetSignal)) {
-  val io = IO(new Bundle {
-    val value = Output(UInt(w.W))
-  })
-  io.value := AsyncWideCounter(w, inc, reset).value
-}
+class WideCounterModule(w: Int, inc: UInt = 1.U, doReset: Boolean = true, clockSignal: Clock = null, resetSignal: Bool = null)
+    extends Module {
+   val io = IO(new Bundle {
+     val value = Output(UInt(w.W))
+   })
+  io.value := {
+    val cc = Option(clockSignal).getOrElse(this.clock)
+    val rr = Option(resetSignal).getOrElse(this.reset)
+    withClockAndReset(cc, rr) { AsyncWideCounter(w, inc, doReset).value }
+  }
+ }
+
 
 object WideCounterModule {
   def apply(w: Int, c: Clock, r: Bool) = {
@@ -84,26 +91,29 @@ object WideCounterModule {
 class WordSync[T <: Data](gen: T, lat: Int = 2) extends Module {
   val size = gen.getWidth
   val io = IO(new Bundle {
-    val in = Flipped(gen.chiselCloneType)
-    val out = gen.chiselCloneType
+    val in = Flipped(chiselTypeOf(gen))
+    val out = chiselTypeOf(gen)
     val tx_clock = Input(Clock())
   })
   val bin2gray = Module(new BinToGray(gen,io.tx_clock))
   bin2gray.io.bin := io.in
   val out_gray = ShiftRegister(bin2gray.io.gray, lat)
-  io.out := gen.cloneType.fromBits(
+  io.out := (
     (0 until size)
-      .map(out_gray.asUInt >> _.U)
-      .reduceLeft((a: UInt, b: UInt) => a^b))
+      .map{ case og => (out_gray.asUInt >> og.U).asUInt }
+      .reduceLeft((a: UInt, b: UInt) => (a^b).asUInt)
+    )
 }
 
-class BinToGray[T <: Data](gen: T, c: Clock) extends Module(_clock = c) {
-  val io = IO(new Bundle {
-    val bin = Flipped(gen.chiselCloneType)
+class BinToGray[T <: Data](gen: T, c: Clock) extends Module {
+   val io = IO(new Bundle {
+    val bin = Flipped(chiselTypeOf(gen))
     val gray = UInt(gen.getWidth.W)
-  })
-  io.gray := Reg(next=(io.bin.asUInt ^ (io.bin.asUInt >> 1.U)))
-}
+   })
+  withClock(c) {
+    io.gray := RegNext(io.bin.asUInt ^ (io.bin.asUInt >> 1.U).asUInt)
+  }
+ }
 
 object WordSync {
   def apply[T <: Data](word: T, c: Clock) = {
